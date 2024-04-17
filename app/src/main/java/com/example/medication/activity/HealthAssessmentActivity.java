31776ee6631102e719f76528fbf61d921a217bfb
LinhNguyen <linhlinhlinh16042002@gmail.com>
2024-04-17 09:54:12 +0700

    ok

--- app/src/main/java/com/example/medication/activity/HealthAssessmentActivity.java
+++ app/src/main/java/com/example/medication/activity/HealthAssessmentActivity.java
@@ -1,14 +1,18 @@
 package com.example.medication.activity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.example.medication.R;
 import com.example.medication.activity.base.MainActivity;
 import com.example.medication.data.Question;
 import com.example.medication.service.AssessmentService;
 import com.example.medication.service.ServiceGenerator;
+import com.example.medication.util.QuestionUtil;
 
-import java.io.IOException;
 import java.util.List;
 
 import retrofit2.Call;
@@ -16,14 +20,22 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class HealthAssessmentActivity extends MainActivity {
+    private LinearLayout listQuestionLayout;
     private final AssessmentService assessmentService = ServiceGenerator.createService(AssessmentService.class);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadListQuestion();
         setContentView(R.layout.health_assessment_activity);
 
         constructor();
+    }
 
+    @Override
+    public void constructor() {
+        super.constructor();
+
+        listQuestionLayout = findViewById(R.id.listQuestionLayout);
     }
 
     private void loadListQuestion() {
@@ -47,6 +59,33 @@ public class HealthAssessmentActivity extends MainActivity {
     }
 
     private void showData(List<Question> questions) {
+        int i = 1;
+//        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.WRAP_CONTENT
+//        );
 
+        for (Question question : questions) {
+            LinearLayout questionLayout = new LinearLayout(this);
+
+            TextView textView = new TextView(this);
+            textView.setText(QuestionUtil.markQuestion(i, question.getContent()));
+            textView.setTextSize(16);
+
+            RadioGroup radioGroup = new RadioGroup(this);
+//            question.getAnswers()
+//                    .forEach(answer -> {
+//                        RadioButton radioButton = new RadioButton(this);
+//                        radioButton.setText(answer.getContent());
+//
+//                        radioGroup.addView(radioButton);
+//                    });
+
+            questionLayout.addView(textView);
+            questionLayout.addView(radioGroup);
+            this.listQuestionLayout.addView(questionLayout);
+
+            i++;
+        }
     }
 }
